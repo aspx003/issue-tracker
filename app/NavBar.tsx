ac07f78312ce94e578e63ad22e506b62d50313bd
Ansuman Patra <ansupatra003@gmail.com>
2024-02-10 08:49:36 +0530

    classnames package added, styled the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,4 +1,7 @@
+"use client";
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { IoMdBug } from "react-icons/io";
 
@@ -7,6 +10,8 @@ const NavBar = () => {
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+
+  const currentPath = usePathname();
 
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
@@ -17,7 +22,11 @@ const NavBar = () => {
         {link.map((link) => (
           <Link
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              "text-zinc-900": currentPath === link.href,
+              "text-zinc-500": currentPath !== link.href,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
             href={link.href}
           >
             {link.label}
